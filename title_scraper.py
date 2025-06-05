3b2264a84b2706e980061590448fa45626617018
Andrew Eckhart <drew@Andrews-MacBook-Air-2.local>
2025-06-05 00:09:19 -0400

    more port changes

--- title_scraper.py
+++ title_scraper.py
@@ -147,7 +147,7 @@ def main():
 if __name__ == "__main__":
 #   main() 
     port = os.environ.get('PORT', 5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
 
 @app.route("/", methods=["GET"])
 def index():
